9feb88f87bec6dd9e882efb905a74ceb30007029
rodgnathaniel <rodgnathaniel@gmail.com>
2019-04-01 13:11:12 -0700

    \

--- django_farm/farm/views.py
+++ django_farm/farm/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse, JsonResponse
 
+from .models import State
+
+import json
+
 # from .models import Game, Mode, State
 
 import json
@@ -32,7 +36,6 @@ def save_state(request):
     coop_price = data['coop_price']
     coop_count = data['coop_count']
                 
-    player = request.user
     state = State(energy=energy, filth=filth, day_light=day_light, milk=milk, milk_val=milk_val, eggs=eggs, eggs_val=eggs_val, bacon=bacon, bacon_val=bacon_val, money=money, wallet=wallet, cow_price=cow_price, cow_count=cow_count, pig_price=pig_price, pig_count=pig_count, coop_price=coop_price, coop_count=coop_count)
     state.save()
     return HttpResponse('hi')

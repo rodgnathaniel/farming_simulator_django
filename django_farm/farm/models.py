d381b1e841265b53d2c46805d9e4da9e46748a59
rodgnathaniel <rodgnathaniel@gmail.com>
2019-04-01 12:41:38 -0700

    setting up database

--- django_farm/farm/models.py
+++ django_farm/farm/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+
+# from django.contrib.auth.models import User
+
+class State(models.Model):
+    player              = models.ForeignKey(User, on_delete=models.CASCADE)
+    duskball_is         = models.BooleanField()
+    haunted_painting_is = models.BooleanField()
+    lucario_is          = models.BooleanField()
+    computer_on         = models.BooleanField()
+    poke_ball_is        = models.BooleanField()
+    left_corner_is      = models.BooleanField()
+    right_corner_is     = models.BooleanField()
+    bookshelf_is        = models.BooleanField()
+    bear_is             = models.BooleanField()
+    move_ball_down      = models.BooleanField()
+    ball_gone           = models.BooleanField()
+    scroll_is           = models.BooleanField()
+    stair_collision     = models.IntegerField()
+    cabnet_collision    = models.IntegerField()
+    rail2_collision     = models.IntegerField()  
+    duskball_collision  = models.IntegerField()
+    painting_collision  = models.IntegerField()
+    haunted_collision   = models.IntegerField()
+    lucario_collision   = models.BooleanField()
+    timestamp           = models.DateTimeField(auto_now_add=True)
--- django_farm/farm/views.py
+++ django_farm/farm/views.py
@@ -9,3 +9,31 @@ import json
 def home_view(request):
 
     return render(request, "farm/farm_sim.html", {})
+
+
+def save_state(request):
+    
+    data = json.loads(request.body)
+    
+        energy = data['energy']
+        filth = data['filth']
+        day_light = data['day_light']
+        milk = data['milk']
+        milk_val = data['milk_val']
+        eggs = data['eggs']
+        eggs_val = data['eggs_val']
+        bacon = data['bacon']
+        bacon_val = data['bacon_val']
+        money = data['money']
+        wallet = data['wallet']
+        cow_price = data['cow_price']
+        cow_count = data['cow_count']
+        pig_price = data['pig_price']
+        pig_count = data['pig_count']
+        coop_price = data['coop_price']
+        coop_count = data['coop_count']
+                
+    player = request.user
+    state = State(energy=energy, filth=filth, day_light=day_light, milk=milk, milk_val=milk_val, eggs=eggs, eggs_val=eggs_val, bacon=bacon, bacon_val=bacon_val, money=money, wallet=wallet, cow_price=cow_price, cow_count=cow_count, pig_price=pig_price, pig_count=pig_count, coop_price=coop_price, coop_count=coop_count)
+    state.save()
+    return HttpResponse('hi')
